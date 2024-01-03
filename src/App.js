79d4483cdaac60d4b325b4edcc5533f6ceafd12c
devdujinkim <devdujinkim@gmail.com>
2024-01-03 23:46:08 +0100

    s

--- src/App.js
+++ src/App.js
@@ -4,40 +4,18 @@ import Button from 'react-bootstrap/Button';
 
 
 function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  //const [username, setUsername] = useState('');
+  //const [password, setPassword] = useState('');
+  //const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [fileList, setFileList] = useState([]);
   const [selectedFile, setSelectedFile] = useState(null);
   const apiURL = "https://www.tajimahalsitdu.it";
   
- // const apiURL = process.env.REACT_APP_API_URL;
-  const handleFileSelect = (fileName) => {
-    setSelectedFile(fileName);
-  };
-  const handleDownload = () => {
-    if (!selectedFile) {
-      alert('파일을 선택해주세요.');
-      return;
-    }
-
-  const confirmDownload = window.confirm(`${selectedFile} 파일을 다운로드하시겠습니까?`);
-  if (confirmDownload) {
-    // 파일 다운로드 로직
-    //const downloadUrl = `${apiURL}/download/${selectedFile}`;
-    const fileName = selectedFile.replace('upload/', '');
-    const downloadUrl = `${apiURL}/download/${fileName}`;
-
-    window.open(downloadUrl, '_blank');
-  }
-  };
-
-
+  
   useEffect(() => {
-    if (isLoggedIn) {
       fetchFileList();
-    }
-  }, [isLoggedIn]);
+    
+  }, [selectedFile]);
 
   const fetchFileList = () => {
     const url = `${apiURL}/files`;
@@ -51,17 +29,27 @@ function App() {
         console.error('Error fetching file list:', error);
       });
   };
-  
 
-  const handleLogin = () => {
-    if (username === 'dudu' && password === 'dudu') {
-      setIsLoggedIn(true);
-    } else {
-      alert('Invalid credentials');
-    }
+ // const apiURL = process.env.REACT_APP_API_URL;
+  const handleFileSelect = (fileName) => {
+    setSelectedFile(fileName);
   };
+  const handleDownload = () => {
+    if (!selectedFile) {
+      alert('파일을 선택해주세요.');
+      return;
+    }
 
- 
+  const confirmDownload = window.confirm(`${selectedFile} Would you like to download the file?`);
+  if (confirmDownload) {
+    // 파일 다운로드 로직
+    //const downloadUrl = `${apiURL}/download/${selectedFile}`;
+    const fileName = selectedFile.replace('upload/', '');
+    const downloadUrl = `${apiURL}/download/${fileName}`;
+
+    window.open(downloadUrl, '_blank');
+  }
+  };
 
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
@@ -78,10 +66,10 @@ function App() {
     .then(response => response.json())
     .then(data => {
       console.log('File uploaded successfully', data);
-      fetchFileList(); // 업로드 성공 후 파일 목록을 새로 불러옵니다.
+      fetchFileList(); 
     })
     .catch(error => console.error('Error:', error));
-  };
+  };  
   
 
   return (
@@ -90,25 +78,6 @@ function App() {
         <h1>Tajimahal Sitdu</h1>
       </header>
       <main className="App-main">
-        {!isLoggedIn ? (
-          <div className="Login">
-            <input
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            {/* <button onClick={handleLogin}>Login</button> */}
-            <button className="button-style" onClick={handleLogin}>Login</button>
-
-          </div>
-        ) : (
           <div className="File-operations">
             <div className="File-list">
               {fileList.map((file, index) => (
@@ -126,7 +95,6 @@ function App() {
             <button className="button-style" onClick={handleDownload}>
               Download Selected File</button>
           </div>
-        )}
       </main>
       <footer className="App-footer">
         <p></p>
