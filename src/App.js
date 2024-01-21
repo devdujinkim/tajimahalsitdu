5c7831502f9587d213e1877d384914c47d5ae7aa
devdujinkim <devdujinkim@gmail.com>
2024-01-21 22:06:20 +0100

    s

--- src/App.js
+++ src/App.js
@@ -9,125 +9,53 @@ function App() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [error, setError] = useState(null);
   const apiURL = "https://api.tajimahalsitdu.it";
-  const [uploadedFileName, ] = useState("");
-  const [formatType, setFormatType] = useState('insert'); // 기본 포맷 타입을 'insert'로 변경
-
   const [todayLogs, setTodayLogs] = useState("");
 
   const fetchTodayLogs = debounce(async () => {
     try {
       const response = await fetch(`${apiURL}/today-logs`);
       const logs = await response.text();
-      setTodayLogs(logs);
+      setTodayLogs(convertLogsToHTML(logs));
     } catch (error) {
       console.error('Error fetching today logs:', error);
       setError(error.message);
     }
   }, 2000);
-  useEffect(() => {
-    fetchTodayLogs();
-  }, []);
-
-  const handleClearLogs = async () => {
-    const password = prompt("Please enter the password to clear logs:");
-    if (password) {
-      try {
-        const response = await fetch(`${apiURL}/clear-logs`, {
-          method: 'DELETE',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ password }),
-        });
-
-        if (response.ok) {
-          setTodayLogs("");
-          console.log('Logs have been cleared');
-        } else {
-          alert('Failed to clear logs: ' + response.status);
-        }
-      } catch (error) {
-        console.error('Error when trying to clear logs:', error);
-        alert('Error when trying to clear logs: ' + error);
-      }
-    } else {
-      alert('Password is required to clear logs');
-    }
-  };
 
   function convertLogsToHTML(logs) {
     return logs.split('\n').map(log => {
       if (log.includes('Country Flag:')) {
         const imageUrl = log.split('Country Flag: ')[1].split(', ')[0];
-        const imageHtml = `<img src="${imageUrl}" alt="Country Flag" style="height:20px;">`;
-        return log.replace(`Country Flag: ${imageUrl}`, imageHtml);
+        return log.replace(`Country Flag: ${imageUrl}`, `<img src="${imageUrl}" alt="Country Flag" style="height:20px;">`);
       }
       return log;
     }).join('<br>');
   }
 
   useEffect(() => {
-    const htmlLogs = convertLogsToHTML(todayLogs);
-    setTodayLogs(htmlLogs);
-  }, [todayLogs]);
+    fetchTodayLogs();
+  }, []);
 
-  const [logData, setLogData] = useState([]);
-
-  const fetchLogData = async () => {
+  const fetchFileList = async () => {
     try {
-      const response = await fetch(`${apiURL}/today-logs`);
+      const response = await fetch(`${apiURL}/files`);
       if (!response.ok) {
-        throw new Error('Today log data fetch failed');
+        throw new Error('Failed to fetch file list');
       }
-      const logs = await response.text();
+      const files = await response.json();
+      setFileList(files.filter(file => file !== 'upload/').map(file => file.replace('upload/', '')));
     } catch (error) {
-      console.error('Error fetching today logs:', error);
+      console.error('Error fetching file list:', error);
       setError(error.message);
     }
   };
 
   useEffect(() => {
-    fetchLogData();
-  }, []);
+    fetchFileList();
+  }, [selectedFile]);
 
-  const handleDelete = async () => {
-    if (!selectedFile) {
-      alert('Please select a file to delete.');
-      return;
-    }
-    const passwordInput = prompt("Please enter the password:");
-    if (passwordInput) {
-      const isPasswordValid = await verifyPassword(passwordInput);
-      if (!isPasswordValid) {
-        alert('Invalid password');
-        return;
-      }
-
-      const confirmDelete = window.confirm(`Are you sure you want to delete the file: ${selectedFile}?`);
-      if (confirmDelete) {
-        // 파일 삭제 요청을 서버로 전송하는 로직
-        try {
-          const response = await fetch(`${apiURL}/delete-file`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ fileName: selectedFile.replace('upload/', '') }), // 'upload/' 접두사 제거
-          });
-          if (!response.ok) {
-            throw new Error('File deletion failed');
-          }
-          console.log('File deleted successfully');
-          fetchFileList(); // 파일 목록을 다시 불러옴
-        } catch (error) {
-          console.error('Error:', error);
-          setError(error.message);
-          alert('Deletion failed: ' + error.message);
-        }
-      }
-    } else {
-      alert('Password required for deleting files.');
-    }
+  const handleFileSelect = (fileName) => {
+    setSelectedFile(fileName);
   };
 
   const verifyPassword = async (inputPassword) => {
@@ -147,44 +75,17 @@ function App() {
     }
   };
 
-  useEffect(() => {
-    fetchFileList();
-  }, [selectedFile]);
-
-  const fetchFileList = async () => {
-    try {
-      const response = await fetch(`${apiURL}/files`);
-      if (!response.ok) {
-        throw new Error('File list fetch failed');
-      }
-      const files = await response.json();
-
-      const filteredFiles = files
-        .filter(file => file !== 'upload/')
-        .map(file => file.replace('upload/', ''));
-
-      setFileList(filteredFiles);
-      setError(null);
-    } catch (error) {
-      console.error('Error fetching file list:', error);
-      setError(error.message);
-    }
-  };
-
-  const handleFileSelect = (fileName) => {
-    setSelectedFile(fileName);
-  };
-
+  // 파일 다운로드 처리 함수
   const handleDownload = async () => {
+    if (!selectedFile) {
+      alert('Please select a file.');
+      return;
+    }
     const passwordInput = prompt("Please enter the password:");
     if (passwordInput) {
       const isPasswordValid = await verifyPassword(passwordInput);
       if (!isPasswordValid) {
         alert('Invalid password');
-        return;
-      }
-      if (!selectedFile) {
-        alert('Please select a file.');
         return;
       }
       const confirmDownload = window.confirm(`Would you like to download the file: ${selectedFile}?`);
@@ -203,6 +104,7 @@ function App() {
     }
   };
 
+  // 파일 업로드 처리 함수
   const handleFileUpload = async (event) => {
     const passwordInput = prompt("Please enter the password:");
     if (passwordInput) {
@@ -236,23 +138,91 @@ function App() {
     }
   };
 
+  // 파일 삭제 처리 함수
+  const handleDelete = async () => {
+    if (!selectedFile) {
+      alert('Please select a file to delete.');
+      return;
+    }
+    const passwordInput = prompt("Please enter the password:");
+    if (passwordInput) {
+      const isPasswordValid = await verifyPassword(passwordInput);
+      if (!isPasswordValid) {
+        alert('Invalid password');
+        return;
+      }
+
+      const confirmDelete = window.confirm(`Are you sure you want to delete the file: ${selectedFile}?`);
+      if (confirmDelete) {
+        try {
+          const response = await fetch(`${apiURL}/delete-file`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ fileName: selectedFile.replace('upload/', '') }),
+          });
+          if (!response.ok) {
+            throw new Error('File deletion failed');
+          }
+          console.log('File deleted successfully');
+          fetchFileList();
+        } catch (error) {
+          console.error('Error:', error);
+          setError(error.message);
+          alert('Deletion failed: ' + error.message);
+        }
+      }
+    } else {
+      alert('Password required for deleting files.');
+    }
+  };
+
+  // 로그 클리어 처리 함수
+  const handleClearLogs = async () => {
+    const password = prompt("Please enter the password to clear logs:");
+    if (password) {
+      try {
+        const response = await fetch(`${apiURL}/clear-logs`, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ password }),
+        });
+
+        if (response.ok) {
+          setTodayLogs("");
+          console.log('Logs have been cleared');
+        } else {
+          alert('Failed to clear logs: ' + response.status);
+        }
+      } catch (error) {
+        console.error('Error when trying to clear logs:', error);
+        alert('Error when trying to clear logs: ' + error);
+      }
+    } else {
+      alert('Password is required to clear logs');
+    }
+  };
+
+  // 코드 포맷터 컴포넌트
   const CodeFormatter = () => {
     const [code, setCode] = useState("");
     const [formattedCode, setFormattedCode] = useState("");
 
-    const handleFormatClick = async () => {
+    const handleFormatClick = () => {
       const transformInsertData = (code) => {
         return code.split('\n').map(line => {
           const rawElements = line.split(/   +/);
           const transformed = rawElements.map(el => {
             return el === 'NULL' ? el : `'${el}'`;
           });
-      
+
           return `(${transformed.join(', ')})`;
         }).join('\n');
       };
-      
-      // Insert 포맷팅 로직
+
       const transformedCode = transformInsertData(code);
       setFormattedCode(transformedCode);
     };
@@ -285,9 +255,8 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <div id="home"></div>
       <header className="App-header">
-        <img src="/33.png" className="image-size" />
+        <img src="/33.png" className="image-size" alt="Header Image" />
       </header>
       <main className="App-main">
         <div className="File-operations">
@@ -303,38 +272,34 @@ function App() {
             ))}
           </div>
           <label htmlFor="file-upload" className="button-style">
-            {uploadedFileName || "Upload"}
+            Upload File
           </label>
+          <input
+            id="file-upload"
+            type="file"
+            onChange={handleFileUpload}
+            style={{ display: 'none' }}
+          />
           <div className="button-container">
-            <input
-              id="file-upload"
-              type="file"
-              onChange={handleFileUpload}
-              style={{ display: 'none' }}
-            />
             <Button className="button-style" onClick={handleDownload}>
               Download Selected File
             </Button>
             <Button className="button-style" onClick={handleDelete}>
               Delete Selected File
             </Button>
-            {error && <div className="error">{error}</div>}
           </div>
         </div>
         <CodeFormatter />
         <div className="log-data">
-          <div>
-            <h2>Today's Logs</h2>
-            <div dangerouslySetInnerHTML={{ __html: todayLogs }} />
-          </div>
+          <h2>Today's Logs</h2>
+          <div dangerouslySetInnerHTML={{ __html: todayLogs }} />
         </div>
+        <footer className="App-footer">
+          <Button onClick={handleClearLogs} className="button-style">Clear Logs</Button>
+        </footer>
       </main>
-      <footer className="App-footer">
-        <p></p>
-        <Button onClick={handleClearLogs} className="button-style">Clear</Button>
-      </footer>
     </div>
   );
-}
-
+            }
+          
 export default App;
--- src/NavBar.js
+++ src/NavBar.js
@@ -3,7 +3,7 @@ import { Link } from "react-scroll";
 
 const NavBar = () => (
   <div className="nav-links">
-    <Link to="home" smooth={true} duration={500}>Home<br></br></Link>
+    <Link to="App" smooth={true} duration={500}>Home<br></br></Link>
     <Link to="File-list" smooth={true} duration={500}>Download<br></br></Link>
     <Link to="code-formatter" smooth={true} duration={500}>Code Formatter</Link>
   </div>
